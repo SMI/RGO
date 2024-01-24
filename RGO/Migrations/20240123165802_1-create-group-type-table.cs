5277fcded8fb6e01471e101b8d4112d540155f55
Suzie Law <slaw002@dundee.ac.uk>
2024-01-24 11:01:39 +0000

    Group_Type table created

---
+++ RGO/Migrations/20240123165802_1-create-group-type-table.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace RGO.Migrations
+{
+    /// <inheritdoc />
+    public partial class _1creategrouptypetable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Group_Types",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(20)", maxLength: 20, nullable: false),
+                    Created_By = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Created_Date = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Updated_By = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Updated_Date = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    Notes = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Group_Types", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Group_Types",
+                columns: new[] { "Id", "Created_By", "Created_Date", "Name", "Notes", "Updated_By", "Updated_Date" },
+                values: new object[,]
+                {
+                    { 1, "seed", new DateTime(2024, 1, 23, 16, 58, 2, 404, DateTimeKind.Local).AddTicks(3116), "Research Group", null, null, null },
+                    { 2, "seed", new DateTime(2024, 1, 23, 16, 58, 2, 404, DateTimeKind.Local).AddTicks(3190), "Data Team", null, null, null }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Group_Types");
+        }
+    }
+}
---
+++ RGO/Models/Group_Type.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace RGO.Models
+{
+    public class Group_Type
+    {
+        [Key]
+        public int Id { get; set; }
+
+
+        [MaxLength(20)]
+        [DisplayName("Group Type Name")]
+        public required string Name { get; set; }
+
+
+        /* Common Columns that should appear on all tables */
+
+        public required string Created_By { get; set; }
+
+        public required DateTime Created_Date { get; set; }
+
+        public string? Updated_By { get; set; }
+
+        public DateTime? Updated_Date { get; set; }
+
+        public string? Notes { get; set; }
+
+
+    }
+}

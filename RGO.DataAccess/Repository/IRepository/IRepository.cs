e4679fb8be03c626e63790abc16be7c9c1b6eb65
Suzie Law <slaw002@dundee.ac.uk>
2024-01-29 15:14:09 +0000

    IRepository and Repository added.  Runs OK

---
+++ RGO.DataAccess/Repository/IRepository/IRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RGO.DataAccess.Repository.IRepository
+{
+    public interface IRepository<TEntity> where TEntity : class
+    {
+        //TEntity - Group Type
+        IEnumerable<TEntity> GetAll();
+        TEntity GetFirstOrDefault(Expression<Func<TEntity, bool>> filter);
+        void Add(TEntity entity);
+        void Remove(TEntity entity);
+        void RemoveRange(IEnumerable<TEntity> entity);
+    }
+}
---
+++ RGO.DataAccess/Repository/Repository.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RGO.DataAccess.Data;
+using RGO.DataAccess.Repository.IRepository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RGO.DataAccess.Repository
+{
+    public class Repository<TEntity> : IRepository<TEntity> where TEntity : class
+    {
+        private readonly ApplicationDbContext _db;
+
+        internal DbSet<TEntity> dbSet;
+
+        public Repository(ApplicationDbContext db)
+        {
+            _db = db;
+            //dbset is generic - will be set to the correct entity type i.e. indicated by TEntity
+            this.dbSet = _db.Set<TEntity>();
+
+        }
+
+        public void Add(TEntity entity)
+        {
+            dbSet.Add(entity);
+        }
+
+        public IEnumerable<TEntity> GetAll()
+        {
+            IQueryable<TEntity> query = dbSet;
+            return query.ToList();
+        }
+
+        public TEntity GetFirstOrDefault(Expression<Func<TEntity, bool>> filter)
+        {
+            IQueryable<TEntity> query = dbSet;
+            query = query.Where(filter);
+            return query.FirstOrDefault();
+        }
+
+        public void Remove(TEntity entity)
+        {
+            dbSet.Remove(entity);
+        }
+
+        public void RemoveRange(IEnumerable<TEntity> entity)
+        {
+            dbSet.RemoveRange(entity);
+        }
+    }
+}

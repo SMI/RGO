6cfaf9670f3c0b8c7e0205e01857a67f40d8b36a
James Friel <JFriel001@dundee.ac.uk>
2024-02-28 08:54:54 +0000

    basic controller for reidentification

---
+++ RGO/Areas/Config/Controllers/ReIdentificationController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Migrations;
+using RGO.DataAccess.Repository.IRepository;
+
+namespace RGO.Areas.Config.Controllers
+{
+    public class ReIdentificationController : Controller
+    {
+
+        private readonly IUnitOfWork _unitOfWork;
+
+        public ReIdentificationController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
+//migrationBuilder.CreateTable(
+//   name: "RGO_ReIdentificationConfiguration",
+//   columns: table => new
+//   {
+//       Id = table.Column<int>(type: "int", nullable: false)
+//           .Annotation("SqlServer:Identity", "1, 1"),
+//       Name = table.Column<string>(type: "nvarchar(255)", maxLength: 255, nullable: false),
+//       Server = table.Column<string>(type: "nvarchar(255)", maxLength: 255, nullable: false),
+//       Database = table.Column<string>(type: "nvarchar(255)", maxLength: 255, nullable: false),
+//       Table = table.Column<string>(type: "nvarchar(255)", maxLength: 255, nullable: false),
+//       DeIdentificationColumn = table.Column<string>(type: "nvarchar(255)", maxLength: 255, nullable: false),
+//       IdentityColumn = table.Column<string>(type: "nvarchar(255)", maxLength: 255, nullable: false),
+//       Username = table.Column<string>(type: "nvarchar(255)", maxLength: 255, nullable: false),
+//       Password = table.Column<string>(type: "nvarchar(255)", maxLength: 255, nullable: false)
+//   },
+//   constraints: table =>
+//   {
+//       table.PrimaryKey("PK_ReIdentificationConfiguration", x => x.Id);
+//   });

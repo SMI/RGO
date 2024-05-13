ec81022dde9baea683f3d27a8637c64efeeeca07
James Friel <JFriel001@dundee.ac.uk>
2024-05-13 09:50:56 +0100

    fix space issue

--- scripts/populate.sql
+++ scripts/populate.sql
@@ -22,6 +22,6 @@ INSERT INTO public."RGO_Dataset_Templates" ("Name", "Created_Date","RGOutput_Id"
 VALUES('MRI Labelling',NOW(),2,1)
 -- Coulmn Template
 INSERT INTO public."RGO_Column_Templates"("RGO_Dataset_TemplateId","Name","PK_Column_Order","Type","Potentially_Disclosive","Created_Date","IsIdentifier","Release_Status_Id")
-VALUES(4,'Image Identifier',1,'Char','N',Now(),0,1)
+VALUES(4,'Image_Identifier',1,'Char','N',Now(),0,1)
 INSERT INTO public."RGO_Column_Templates"("RGO_Dataset_TemplateId","Name","PK_Column_Order","Type","Potentially_Disclosive","Created_Date","IsIdentifier","Release_Status_Id")
 VALUES(4,'MRI_Classification_consensus',1,'int','N',Now(),0,1)

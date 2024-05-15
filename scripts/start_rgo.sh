81e1611fed6789a1feb8974c799feceb6ff1b046
howff <howff>
2024-05-15 15:32:02 +0100

    container scripts - add persistent volume for postgres and add two stop scripts

--- scripts/start_rgo.sh
+++ scripts/start_rgo.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
-exposeport=8081
-# Find out which IP address postgres is using inside the docker bridge network
-pgip=$( docker network inspect bridge | jq -r '.[0].Containers[]|.IPv4Address' | sed 's,/.*,,')
-docker run --name rgo-app -e "DatabaseType=Postgres" -e "ConnectionStrings:DefaultConnection=Host=${pgip},5433; Database=R-GO; Username=postgres; Password=postgres;" --expose ${exposeport} -p ${exposeport}:8080 taggenblu/rgo:0.0.3
 
+exposeport=8081
+
+pgip=$(sudo docker network inspect bridge | jq -r '.[0].Containers[]|.IPv4Address' | sed 's,/.*,,')
+echo "Postgres IP address: $pgip"
+
+echo "Starting RGO, use Ctrl-C to stop"
+sudo docker run --name rgo-app -e "DatabaseType=Postgres" -e "ConnectionStrings:DefaultConnection=Host=${pgip}:5432; Database=R-GO; Username=postgres; Password=mysecretpassword;" --expose ${exposeport} -p ${exposeport}:8080 taggenblu/rgo:0.0.5
---
+++ scripts/stop_postgres.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+sudo docker stop postgres-rgo
+# If you also want to remove it:
+sudo docker rm postgres-rgo

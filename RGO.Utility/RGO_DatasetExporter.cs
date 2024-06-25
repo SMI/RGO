2a69e81dcbd79b0560d631990b18206c122973d1
James Friel <JFriel001@dundee.ac.uk>
2024-06-25 09:41:36 +0100

    tidy up

--- RGO.Utility/RGO_DatasetExporter.cs
+++ RGO.Utility/RGO_DatasetExporter.cs
@@ -78,43 +78,6 @@ public class RGO_DatasetExporter
             sb.AppendLine(string.Join(",", fields));
         }
         return sb.ToString();
-        //var datasetTemplate = _unitOfWork.RGO_Dataset_Template.GetAll().Where(t => t.Id == _dataset.RGO_Dataset_TemplateId).First();
-        //var records = _unitOfWork.RGO_Record.GetAll().Where(r => r.RGO_DatasetId == _dataset.Id);
-        //var recordIds = records.Select (r => r.Id).ToList();
-        ////var columns = _unitOfWork.RGO_Column.GetAll().Where(c => records.Select(s => s.Id).Contains(c.RGO_RecordId));
-        //var columns = _unitOfWork.RGO_Column.GetAll().Where(c => recordIds.Contains(c.RGO_RecordId)).DistinctBy(c => c.Id).ToList();
-        //var columnNames = columns.Select(c => c.Name).Distinct();
-
-        //var orderedRecords = new Dictionary<int, Dictionary<string, string>>();
-        //foreach (var column in columns)
-        //{
-        //    orderedRecords.TryGetValue(column.RGO_RecordId, out var foundRecord);
-        //    if (foundRecord is null)
-        //    {
-        //        foundRecord = new Dictionary<string, string>();
-        //    }
-        //    foundRecord.Add(column.Name, column.Column_Value);
-        //    orderedRecords[column.RGO_RecordId] = foundRecord;
-        //}
-        //var columnStrings = string.Join(",", columns.Select(c => c.Name).Distinct());
-        //var recordStrings = new List<string>();
-        //foreach(var recordId in orderedRecords.Keys)
-        //{
-        //    var record = orderedRecords[recordId];
-        //    var newRecord = new List<string>();
-        //    foreach(var column in columnNames) {
-        //        //to do munge
-        //        newRecord.Add(record[column]);
-        //    }
-        //    recordStrings.Add(string.Join(",", newRecord));
-        //}
-        //var returnString = columnStrings;
-        //foreach(var r in recordStrings)
-        //{
-        //    returnString = returnString + Environment.NewLine + r.ToString();
-        //}
-        //return returnString;
-        return "";
     }
 }
 
